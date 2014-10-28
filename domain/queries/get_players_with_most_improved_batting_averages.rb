b35491637431cf464fb9f10c1afb70a0f824f3e7
Darren Smith <ddarrensmith@gmail.com>
2014-10-28 14:03:49 -0500

    got the test framework in place

--- domain/queries/get_players_with_most_improved_batting_averages.rb
+++ domain/queries/get_players_with_most_improved_batting_averages.rb
@@ -3,13 +3,18 @@ require "CSV"
 module Domain
   module Queries    
     class GetPlayersWithMostImprovedBattingAverages
+      attr_writer :csv
       
       def execute(from_year, to_year)
-        CSV.foreach("#{$DATA_PATH}/batting.csv") do |row|
+        csv.foreach("#{$DATA_PATH}/batting.csv") do |row|
           puts row.to_s
         end
       end
       
+      private
+      def csv
+        @csv ||= CSV
+      end
     end      
   end
 end
